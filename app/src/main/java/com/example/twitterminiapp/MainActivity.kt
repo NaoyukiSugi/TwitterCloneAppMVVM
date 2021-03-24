c5020f1d6aa7d280f9d2a7c9f86c83bd4efddff0
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-24 20:50:21 +0900

    use viewBinding

--- app/src/main/java/com/example/twitterminiapp/MainActivity.kt
+++ app/src/main/java/com/example/twitterminiapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.twitterminiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.twitterminiapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
