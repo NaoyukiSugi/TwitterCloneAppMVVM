1d98da8d409d6265ebfb3adf728aed91f445a6e8
shishamo <bnsgny@gmail.com>
2021-03-24 12:06:49 +0900

    add RemoteDataSource (#4)

---
+++ app/src/main/java/com/example/twitterminiapp/data/repository/dataSourceImpl/TwitterRemoteDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.example.twitterminiapp.data.repository.dataSourceImpl
+
+import com.example.twitterminiapp.data.api.TwitterAPIService
+import com.example.twitterminiapp.data.model.Tweet
+import com.example.twitterminiapp.data.repository.dataSource.TwitterRemoteDataSource
+import retrofit2.Response
+
+class TwitterRemoteDataSourceImpl(
+    private val apiService: TwitterAPIService
+) : TwitterRemoteDataSource {
+
+    override suspend fun getTimeline(): Response<List<Tweet>> {
+        return apiService.getTimeline()
+    }
+}
