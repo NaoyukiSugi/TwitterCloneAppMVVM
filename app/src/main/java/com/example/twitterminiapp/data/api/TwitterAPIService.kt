ab1c008482e1b7844a0dad5298fcdcb5ab00e1dc
shishamo <bnsgny@gmail.com>
2021-03-25 20:14:26 +0900

    fix header (#16)

--- app/src/main/java/com/example/twitterminiapp/data/api/TwitterAPIService.kt
+++ app/src/main/java/com/example/twitterminiapp/data/api/TwitterAPIService.kt
@@ -1,13 +1,11 @@
 package com.example.twitterminiapp.data.api
 
-import com.example.twitterminiapp.BuildConfig
 import com.example.twitterminiapp.data.model.Tweet
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
+
 
 interface TwitterAPIService {
-    @Headers(BuildConfig.OAUTH_HEADER_STRING)
     @GET("1.1/statuses/home_timeline.json")
     suspend fun getTimeline(): Response<List<Tweet>>
 }
--- app/src/main/java/com/example/twitterminiapp/presentation/di/RetrofitModule.kt
+++ app/src/main/java/com/example/twitterminiapp/presentation/di/RetrofitModule.kt
@@ -1,9 +1,13 @@
 package com.example.twitterminiapp.presentation.di
 
+import com.example.twitterminiapp.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,10 +18,32 @@ class RetrofitModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit =
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(Interceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader(
+                        "Authorization",
+                        BuildConfig.OAUTH_HEADER_STRING
+                    )
+                    .build()
+                return@Interceptor chain.proceed(newRequest)
+            })
+            .addInterceptor(interceptor)
+            .build()
+        return client
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .build()
 
     private companion object {
