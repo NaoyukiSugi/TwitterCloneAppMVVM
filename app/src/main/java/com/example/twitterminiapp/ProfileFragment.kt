a8ee1e40f4da38fde4730eb4374203637d0d0417
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-26 15:43:09 +0900

    update

--- app/src/main/java/com/example/twitterminiapp/ProfileFragment.kt
+++ app/src/main/java/com/example/twitterminiapp/ProfileFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.twitterminiapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var viewBinding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        viewBinding = FragmentProfileBinding.inflate(inflater, container, false)
+        return viewBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args: ProfileFragmentArgs by navArgs()
+        viewBinding.textView.text = args.selectedUserIcon.toString()
+    }
 }
