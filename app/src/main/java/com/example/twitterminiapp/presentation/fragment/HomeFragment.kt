ce057221397bf03b3fb536df83ca5a102ad7194a
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-29 22:31:00 +0900

    change to databinding

--- app/src/main/java/com/example/twitterminiapp/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/twitterminiapp/presentation/fragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.twitterminiapp.R
@@ -17,17 +18,17 @@ import com.example.twitterminiapp.presentation.viewmodel.TwitterViewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var viewBinding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var viewModel: TwitterViewModel
     private lateinit var twitterAdapter: TwitterAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        viewBinding = FragmentHomeBinding.inflate(inflater, container, false)
-        return viewBinding.root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,9 +57,9 @@ class HomeFragment : Fragment() {
                 is Resource.Error -> {
                     resource.message?.let {
                         Toast.makeText(
-                                activity,
-                                "An error occured: $it",
-                                Toast.LENGTH_LONG
+                            activity,
+                            "An error occured: $it",
+                            Toast.LENGTH_LONG
                         ).show()
                     }
                 }
@@ -70,7 +71,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun initRecyclerView() {
-        viewBinding.homeTimelineRecyclerView.apply {
+        binding.homeTimelineRecyclerView.apply {
             adapter = twitterAdapter
             layoutManager = LinearLayoutManager(activity)
         }
