3d9ce4fa8fd0745cd989623d52d86e421d79d511
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-31 14:28:55 +0900

    add setOnQueryTextListener in BindingAdapter

--- app/src/main/java/com/example/twitterminiapp/presentation/dataBinding/BindingAdapters.kt
+++ app/src/main/java/com/example/twitterminiapp/presentation/dataBinding/BindingAdapters.kt
@@ -1,8 +1,12 @@
 package com.example.twitterminiapp.presentation.dataBinding
 
+import android.widget.SearchView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.twitterminiapp.presentation.viewmodel.TwitterViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 object BindingAdapters {
 
@@ -16,5 +20,24 @@ object BindingAdapters {
             viewModel.getTimeline()
             swipeRefreshLayout.isRefreshing = false
         }
+    }
+
+    @BindingAdapter("onQueryTextListener")
+    @JvmStatic
+    fun setOnQueryTextListener(
+        searchView: SearchView,
+        viewModel: TwitterViewModel
+    ) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.getSearchedTimeline(query.toString())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // do nothing
+                return false
+            }
+        })
     }
 }
